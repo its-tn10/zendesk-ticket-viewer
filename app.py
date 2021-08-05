7c92e51a38a240d9d6ce56e0b8c7440a9a9de6f4
Ubuntu <ubuntu@ip-172-31-63-174.ec2.internal>
2021-08-05 05:51:41 +0000

    Implemented exception handling for the routes

--- app.py
+++ app.py
@@ -13,16 +13,22 @@ def index():
 @app.route('/view/', defaults={'page': 1})
 @app.route('/view/<int:page>')
 def view(page):
-    tickets = utils.get_page(page)
+    try:
+        tickets = utils.get_page(page)
 
-    return render_template('table.html', tickets=tickets[1], pages=tickets[0])
+        return render_template('table.html', tickets=tickets[1], pages=tickets[0])
+    except Exception as error:
+        return render_template('error.html', error=str(error))
 
 @app.route('/ticket/<int:ticket_id>')
 def ticket(ticket_id):
-    ticket = utils.get_ticket(ticket_id)
-    ticket['created_at'] = utils.parse_timestamp(ticket['created_at'], '%m/%d/%Y, %H:%M:%S')
+    try:
+        ticket = utils.get_ticket(ticket_id)
+        ticket['created_at'] = utils.parse_timestamp(ticket['created_at'], '%m/%d/%Y, %H:%M:%S')
 
-    return render_template('ticket.html', ticket=ticket)
+        return render_template('ticket.html', ticket=ticket)
+    except Exception as error:
+        return render_template('error.html', error=str(error))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('SERVER_HOST'), port=os.environ.get('SERVER_PORT'), debug=True)
